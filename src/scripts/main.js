993a78b89b1cfb5c9a8e33c939167e30de189522
Brett Hughes <twofivinclimber@gmail.com>
2022-05-17 18:43:51 -0500

    modularizing SCSS in NPM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,9 @@
+import houses from './houses';
+import '../styles/main.scss';
+// import './body.scss';
+
 const students = [];
 const voldysArmy = [];
-const houses = [
-  {
-    house: 'gryffindor',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/1/16/Gryffindor_crest.png',
-  },
-  {
-    house: 'slytherin',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/45/Slytherin_Crest.png',
-  },
-  {
-    house: 'hufflepuff',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png',
-  },
-  {
-    house: 'ravenclaw',
-    crest:
-      'https://static.wikia.nocookie.net/pottermore/images/4/4f/Ravenclaw_crest.png',
-  },
-];
 
 const renderToDOM = (divId, content) => {
   const selectedDiv = document.querySelector(divId);
@@ -85,11 +67,17 @@ const studentsOnDom = (divId, array, house = 'Hogwarts') => {
     domString += `
     <div class='card bg-dark text-white'>
       <img src='${
-  divId === '#voldy' ? 'https://carboncostume.com/wordpress/wp-content/uploads/2019/10/deatheater-harrypotter.jpg' : student.crest}' 
+  divId === '#voldy'
+    ? 'https://carboncostume.com/wordpress/wp-content/uploads/2019/10/deatheater-harrypotter.jpg'
+    : student.crest}' 
           class='card-img' alt='${student.house} crest'>
       <div class='card-img-overlay'>
         <h5 class='card-title'>${student.name}</h5>
-        ${divId} === '#voldy' ? '<p class='card-text'>Death Eater</p>' : '<p class='card-text'>${student.house}</p>'; <button type='button' id='expel--${student.id}' class='btn btn-danger btn-sm'>Expel</button>`;
+        ${divId} === '#voldy' ? '<p class='card-text'>Death Eater</p>' : '<p class='card-text'>${
+  student.house
+}</p>'; <button type='button' id='expel--${
+  student.id
+}' class='btn btn-danger btn-sm'>Expel</button>`;
   });
   renderToDOM(divId, domString);
 };
